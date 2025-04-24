c9a17522aa9f7a19f91ee3cf6f8d2337d34123e2
rosicenter691 <rosicenter691@gmail.com>
2025-04-24 15:19:44 +0700

    Update kyt.sh

--- Bot/kyt.sh
+++ Bot/kyt.sh
@@ -19,13 +19,13 @@ apt update && apt upgrade
 apt install neofetch -y
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://nata.serv00.net/v1.8/Bot/bot.zip
+wget https://github.com/rosicenter691/sc-vip/main/Bot/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://nata.serv00.net/v1.8/Bot/kyt.zip
+wget https://github.com/rosicenter691/sc-vip/main/Bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
