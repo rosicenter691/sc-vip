348452a4d2d855eeac2a4b8d6ca2ed3c94b3adc1
rosicenter691 <rosicenter691@gmail.com>
2025-05-31 08:13:49 +0700

    Update slowdns.sh

--- Cfg/slowdns.sh
+++ Cfg/slowdns.sh
@@ -10,7 +10,7 @@ REDBG="\033[41;37m"
 NC='\e[0m'
 REPOS="https://raw.githubusercontent.com/rosicenter691/sc-vip/main/"
 ns_domain_cloudflare() {
-	DOMAIN="xdg-boost.my.id"
+	DOMAIN="rosicenter5.my.id"
 	DOMAIN_PATH=$(cat /etc/xray/domain)
 	echo -e "${BLUE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 	echo -e "           Masukan Subdomain              "
@@ -20,10 +20,10 @@ ns_domain_cloudflare() {
 	if [ -z $SUB ]; then
     exit
     else
-	SUB_DOMAIN=${SUB}."xdg-boost.my.id"
+	SUB_DOMAIN=${SUB}."rosicenter5.my.id"
 	NS_DOMAIN=dns.${SUB_DOMAIN}
-	CF_ID=ziexyuan@gmail.com
-        CF_KEY=defec9840c857888b6d1986354d45d617c83a
+	CF_ID=panwaslublegapilkada@gmail.com
+        CF_KEY=c1feca5207e3db081a8d69eae9a5015ec5d4f
 	set -euo pipefail
 	IP=$(wget -qO- ipinfo.io/ip)
 	echo "Updating DNS NS for ${NS_DOMAIN}..."
